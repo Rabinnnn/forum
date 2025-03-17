e1b4a4e42d51d5a64fa568764564f1626b94673c
kevwasonga <kevinwasonga116@gmail.com>
2025-03-18 02:44:59 +0300

    update error handling

--- internal/auth/model.go
+++ internal/auth/model.go
@@ -1,1 +1,12 @@
 package auth
+
+import "database/sql"
+
+// User represents a forum user
+type User struct {
+    ID         string
+    Username   string
+    Email      string
+    Password   string
+    ProfilePic sql.NullString
+}
