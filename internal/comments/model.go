43657e88a9a0a8d3a12f1a44eaa0799377c20960
kevwasonga <kevinwasonga116@gmail.com>
2025-03-19 14:01:10 +0300

    Add Comment struct for handling comment data

--- internal/comments/model.go
+++ internal/comments/model.go
@@ -1,1 +1,16 @@
 package comments
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Comment struct {
+	ID        string    `json:"id"`
+	PostID    string    `json:"post_id"`
+	UserID    string    `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	Username  string    `json:"username"`
+	ProfilePic sql.NullString `json:"profile_pic"`
+}
