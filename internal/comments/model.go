f3b1a30771816c55f86a3000ae6d520b0e8ec9d5
kevwasonga <kevinwasonga116@gmail.com>
2025-03-19 14:59:35 +0300

    Feat:  populate comment struct with all necessary fields including ID and creation time

--- internal/comments/model.go
+++ internal/comments/model.go
@@ -6,11 +6,11 @@ import (
 )
 
 type Comment struct {
-	ID        string    `json:"id"`
-	PostID    string    `json:"post_id"`
-	UserID    string    `json:"user_id"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"created_at"`
-	Username  string    `json:"username"`
+	ID         string         `json:"id"`
+	PostID     string         `json:"post_id"`
+	UserID     string         `json:"user_id"`
+	Content    string         `json:"content"`
+	CreatedAt  time.Time      `json:"created_at"`
+	Username   string         `json:"username"`
 	ProfilePic sql.NullString `json:"profile_pic"`
 }
