5f2e50418144a85d45fe8e5178e6d6fe65752eca
kevwasonga <kevinwasonga116@gmail.com>
2025-03-24 10:40:02 +0300

    - Add  custom structs to  populate likes fields

--- internal/likes/model.go
+++ internal/likes/model.go
@@ -1,1 +1,17 @@
 package likes
+
+import "time"
+
+type Like struct {
+    ID        string    `json:"id"`
+    UserID    string    `json:"user_id"`
+    PostID    string    `json:"post_id"`
+    IsLike    bool      `json:"is_like"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+type LikeResponse struct {
+    Likes    int    `json:"likes"`
+    Dislikes int    `json:"dislikes"`
+    Error    string `json:"error,omitempty"`
+}
